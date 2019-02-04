de080e5962f8055f7dcd0dc3791f8de6ba279d1b
ftemplin <fritz.templin@gmail.com>
2019-02-04 10:34:43 -0800

    created simple JavaScript/comparisonCal.js to evaluate statements, currently evaluating variables as strings not numbers

--- JavaScript/comparisonCalc/comparisonCalc.js
+++ JavaScript/comparisonCalc/comparisonCalc.js
@@ -2,4 +2,20 @@ alert("Conneted to comparisonCalc.js");
 var x = prompt("Enter value 1");
 var y = prompt("Enter value 2");
 var comp = prompt("Enter comparison operator");
-alert(x + comp + y);
+
+var operators = {
+    '+': function(a, b) { return a + b },
+    '<': function(a, b) { return a < b },
+    '>': function(a, b) { return a > b },
+    '%': function(a, b) { return a % b },
+    '-': function(a, b) { return a - b },
+    '*': function(a, b) { return a * b },
+    '=': function(a, b) { return a = b },
+    '==': function(a, b) { return a == b },
+    '!=': function(a, b) { return a != b },
+    '===': function(a, b) { return a === b },
+     // ... more operators can be added as needed
+};
+
+
+alert(operators[comp](x, y));
