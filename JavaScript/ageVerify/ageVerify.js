651a750c84dd2f8eb3022bea17431c9ec02e9c39
ftemplin <fritz.templin@gmail.com>
2019-02-04 14:43:38 -0800

    Added input fields and function to ageVerify.js but it is not working correctly

--- JavaScript/ageVerify/ageVerify.js
+++ JavaScript/ageVerify/ageVerify.js
@@ -1,5 +1,5 @@
 alert("connected to ageVerify.js");
-var age = prompt("What is your age");
+// var age = prompt("What is your age");
 
 
 while(isNaN(age) || age < 0){
@@ -23,4 +23,37 @@ else if( age = 21){
 } 
 else{
 	alert("You may enter and drink");
+}
+
+// not working correctly--------------------------------
+function checkAge() {
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("age").value;
+
+  // If x is Not a Number or less than zero
+  while (isNaN(x) || x < 0) {
+    text = "Input not valid";
+  }
+  if(age < 18){
+	text = "You may not enter";
+	if(age%2 != 0){
+		text ="your age is odd!";
+	}
+	else{
+		text ="your age is even";
+	}
+} 
+else if( age < 21){
+	text ="you may enter but not drink";
+} 
+else if( age = 21){
+	text ="Happy Birthday you get a free drink";
+} 
+else{
+	text ="You may enter and drink";
+} 
+  
+  document.getElementById("demo").innerHTML = text;
 }
