bf043c458c1cf7938d6ab32cabbc72c0906b0141
ftemplin <Fritz.templin@gmail.com>
2019-02-10 10:10:07 -0800

    adding capitalize function

--- JavaScript/functions/function.js
+++ JavaScript/functions/function.js
@@ -6,11 +6,20 @@ function myFunction (){
 	document.write("Have a nice day!");
 }
 
-function square(num){
-	var num = prompt("Enter a number");
-	var result = square(num);
-	document.getElementById("demo").innerHTML = "result";
-}
+// function square (){
+// 	alert("Square function executed");
+// 	// var num = document.querySelector('numberInput').value
+// 	// var result = square(num);
+// 	// document.getElementById("demo").innerHTML = "result";
+// 	document.write("your number is" + result.value;
+// }
 
 // var num = 10;
 // var squared = square(num)
+
+function square (){
+	alert("Square Function");
+	var num = document.querySelector('numberInput').value
+	document.write(num);
+	// document.write("Have a nice day!");
+}
