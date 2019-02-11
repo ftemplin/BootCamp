025a470e93270aa18b0a3b2bb634d3bcf85f588d
ftemplin <Fritz.templin@gmail.com>
2019-02-10 20:51:42 -0800

    added prompt for todo list adn newtodo array

--- JavaScript/TodoList_1/todoList.js
+++ JavaScript/TodoList_1/todoList.js
@@ -1,4 +1,12 @@
 console.log("todoList.js is connected");
-var todos [];
+var todos = ["Walk Dog"];
 
 var input = prompt("What would you like to do?")
+
+if(input === "list"){
+	console.log(todos);
+}else if(input === "new"){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+}
