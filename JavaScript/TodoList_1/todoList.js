1f3dbac9f45e4eddc7ce2c3a6c25d2f3a2161ee9
ftemplin <Fritz.templin@gmail.com>
2019-02-10 21:03:11 -0800

    Added quit app and While loop, function is buggy

--- JavaScript/TodoList_1/todoList.js
+++ JavaScript/TodoList_1/todoList.js
@@ -1,12 +1,15 @@
 console.log("todoList.js is connected");
 var todos = ["Walk Dog"];
 
-var input = prompt("What would you like to do?")
-
-if(input === "list"){
-	console.log(todos);
-}else if(input === "new"){
-	//ask for new todo
-	var newTodo = prompt("Enter new todo");
-	todos.push(newTodo);
+var input = prompt("What would you like to do?");
+while(input !== "quit"){
+	if(input === "list"){
+		console.log(todos);
+	}else if(input === "new"){
+		//ask for new todo
+		var newTodo = prompt("Enter new todo");
+		todos.push(newTodo);
+	}
+	input = prompt("What would you like to do?");
 }
+console.log("Quiting the App, Goodbye :)")
