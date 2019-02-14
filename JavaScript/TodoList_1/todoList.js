7b1034a8829aa8e09c4f5c03308bc036b738c852
ftemplin <Fritz.templin@gmail.com>
2019-02-13 20:42:39 -0800

    Added notifications of new and delete also refactored fuinctions new, list and deleteTodo

--- JavaScript/TodoList_1/todoList.js
+++ JavaScript/TodoList_1/todoList.js
@@ -4,19 +4,32 @@ var todos = ["Walk Dog"];
 var input = prompt("What would you like to do?");
 while(input !== "quit"){
 	if(input === "list"){
-		todos.forEach(function(todo, i){
-		console.log(todo + " : " + i);
-	});
+		listTodos();
 	}else if(input === "new"){
-		//ask for new todo
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		newTodo();
 	}else if(input ==="delete"){
-		// ask for index of item to delete
-		var index = prompt("Enter the index of item to delete.");
-		//delete item with matching index
-		todos.splice(index,1);
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 console.log("Quiting the App, Goodbye :)")
+
+function listTodos(){
+	todos.forEach(function(todo, i){
+	console.log(todo + " : " + i);
+});
+}
+function newTodo(){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added item ")
+}
+
+function deleteTodo(){
+	// ask for index of item to delete
+		var index = prompt("Enter the index of item to delete.");
+		//delete item with matching index
+		todos.splice(index,1);
+		console.log("Deleted item ")
+}
