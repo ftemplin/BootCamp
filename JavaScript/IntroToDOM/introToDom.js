8ffdb33624e845c334f70b622126ff376ad62037
ftemplin <Fritz.templin@gmail.com>
2019-02-23 20:34:25 -0800

    Set up javascript dom manipulation in introToDom.html

--- JavaScript/IntroToDOM/introToDom.js
+++ JavaScript/IntroToDOM/introToDom.js
@@ -1,1 +1,19 @@
 console.log("Connected to introToDom.js");
+
+var h3 = document.querySelector("h3");
+
+h3.style.color = "green";
+
+var body = document.querySelector("body");
+var isBlue = false;
+var count = 0;
+setInterval(function(){
+	if (isBlue && count < 10) {
+		body.style.background = "white";
+		count ++
+
+	}else{
+		body.style.background = "Blue";
+	}
+	isBlue = !isBlue;
+}, 1000);
