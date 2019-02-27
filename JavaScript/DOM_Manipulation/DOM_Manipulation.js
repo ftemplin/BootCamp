fb6e6492370eb5e8a45075a7df921a4cac5710de
ftemplin <Fritz.templin@gmail.com>
2019-02-26 18:18:34 -0800

    setting up DOM_Manipulation.js background color changer

--- JavaScript/DOM_Manipulation/DOM_Manipulation.js
+++ JavaScript/DOM_Manipulation/DOM_Manipulation.js
@@ -3,6 +3,17 @@ console.log("Conected to DOM_Manipulation.js")
 document.querySelector("#content h1").addEventListener("click", function(){
 alert("you clicked the h1")
 });
+var isPurple = false;
+document.querySelector("#content2 button").addEventListener("click", function(){
+	//alert("clicked")
+	//if(#content )
+	if(isPurple){
+		document.body.style.background = "white";
+	}else{
+		document.body.style.background = "purple";
+	}
+	
+});
 
 
 
