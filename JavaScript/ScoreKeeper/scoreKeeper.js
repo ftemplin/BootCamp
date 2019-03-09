4bee51e4222a0466e888b1ad67e70ccf31438857
ftemplin <Fritz.templin@gmail.com>
2019-03-08 22:33:29 -0800

    scoreKeeper.js: Added logic to stop score if win is equal to player scores

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -6,17 +6,33 @@ var p1Score = 0;
 var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var gameOver = false;
+var win = 5;
 
 
 p1Button.addEventListener("click", function(){
 	//alert("p1 clicked!");
 	//console.log("p1Score");
-	p1Score++;
-	p1Display.textContent = p1Score;
+	if(!gameOver){
+		p1Score++;
+		if(p1Score === win){
+			gameOver = true;
+			//console.log("Game Over!");
+		}
+		p1Display.textContent = p1Score;
+	}
+	
 });
 
 p2Button.addEventListener("click", function(){
 	//alert("p2 clicked!");
-	p2Score++;
-	p2Display.textContent = p2Score;
+
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === win){
+			gameOver = true;
+			//console.log("Game Over!");
+		}
+		p2Display.textContent = p2Score;
+	}
 });
