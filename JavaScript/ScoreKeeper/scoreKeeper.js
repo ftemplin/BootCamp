b2f127d1f8d5d4f5c7dcd23626628f6640b22be1
ftemplin <Fritz.templin@gmail.com>
2019-03-08 22:02:00 -0800

    Added palyer2 eventlistener scoreKeeper.js, also added p1Score counter, h1 variable

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -1,8 +1,17 @@
-alert("connected to scoreKeeper.js");
+//alert("connected to scoreKeeper.js");
 
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var p1Score = 0;
+var h1 = document.querySelector("h1")
 
 p1Button.addEventListener("click", function(){
-	alert("p1 clicked!");
+	//alert("p1 clicked!");
+	console.log("p1Score");
+	p1Score++;
+	h1.tectContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+	alert("p2 clicked!");
 });
