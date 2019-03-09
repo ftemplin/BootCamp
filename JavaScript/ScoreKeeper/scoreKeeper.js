e0f50e04f43e47e6770949fb309aa1008049f5f5
ftemplin <Fritz.templin@gmail.com>
2019-03-09 10:45:56 -0800

    scoreKeeper.js: added reset button functionality Website1/careers.html: added inputs for candidate submission form

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -8,6 +8,7 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var gameOver = false;
 var win = 5;
+var resetButton = document.querySelector("#reset");
 
 
 p1Button.addEventListener("click", function(){
@@ -37,4 +38,13 @@ p2Button.addEventListener("click", function(){
 		}
 		p2Display.textContent = p2Score;
 	}
+});
+
+resetButton.addEventListener("click", function(){
+	var p1Score = 0;
+	p1Display.textContent = p1Score;
+	p1Display.classList.remove("winner");
+	var p2Score = 0;
+	p2Display.textContent = p2Score;
+	gameOver = false;
 });
