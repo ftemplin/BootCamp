44c390dae68c7baba6c2184fbd1637993bd6c0bd
ftemplin <Fritz.templin@gmail.com>
2019-03-09 11:37:07 -0800

    scoreKeeper.js: added reset as function, added player input of value of winning score win variable

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -1,14 +1,17 @@
-//alert("connected to scoreKeeper.js");
+// alert("connected to scoreKeeper.js");
 
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var p1Score = 0;
-var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var resetButton = document.querySelector("#reset");
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
+
 var gameOver = false;
 var win = 5;
-var resetButton = document.querySelector("#reset");
+var p1Score = 0;
+var p2Score = 0;
 
 
 p1Button.addEventListener("click", function(){
@@ -41,6 +44,10 @@ p2Button.addEventListener("click", function(){
 });
 
 resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
 	p1Score = 0;
 	p1Display.textContent = p1Score;
 	p1Display.classList.remove("winner");
@@ -48,4 +55,12 @@ resetButton.addEventListener("click", function(){
 	p2Display.textContent = p2Score;
 	p2Display.classList.remove("winner");
 	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	//alert("numb input changed");
+	p.textContent = ("Playing to: " + numInput.value)
+	win = Number(numInput.value);
+	reset();
+
 });
