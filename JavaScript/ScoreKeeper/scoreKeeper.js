21bd5d213279e23b400eacbfb37bfac172db2e15
ftemplin <Fritz.templin@gmail.com>
2019-03-08 21:57:12 -0800

    Added query selector and event listener for Player1

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -1,1 +1,8 @@
 alert("connected to scoreKeeper.js");
+
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+
+p1Button.addEventListener("click", function(){
+	alert("p1 clicked!");
+});
