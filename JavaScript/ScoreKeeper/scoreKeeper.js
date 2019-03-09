cd31f0939b6871b8948d46051393b28976eb3417
ftemplin <Fritz.templin@gmail.com>
2019-03-08 22:22:43 -0800

    scoreKeeper.js: added player2 same as player1

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -3,7 +3,9 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var p1Score = 0;
+var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 
 
 p1Button.addEventListener("click", function(){
@@ -14,5 +16,7 @@ p1Button.addEventListener("click", function(){
 });
 
 p2Button.addEventListener("click", function(){
-	alert("p2 clicked!");
+	//alert("p2 clicked!");
+	p2Score++;
+	p2Display.textContent = p2Score;
 });
