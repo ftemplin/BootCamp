b5f63780f9f1ce4004b1461cc326a9d6f7fa36c6
ftemplin <Fritz.templin@gmail.com>
2019-03-08 22:18:56 -0800

    scoreKeeper.js: added p1Display span to select only player 1 score for update

--- JavaScript/ScoreKeeper/scoreKeeper.js
+++ JavaScript/ScoreKeeper/scoreKeeper.js
@@ -3,13 +3,14 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var p1Score = 0;
-var h1 = document.querySelector("h1")
+var p1Display = document.querySelector("#p1Display");
+
 
 p1Button.addEventListener("click", function(){
 	//alert("p1 clicked!");
-	console.log("p1Score");
+	//console.log("p1Score");
 	p1Score++;
-	h1.textContent = p1Score;
+	p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function(){
