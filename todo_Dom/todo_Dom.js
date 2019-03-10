ef3a5565d9a0f7932686792a19707939755f4539
ftemplin <Fritz.templin@gmail.com>
2019-03-09 20:53:12 -0800

    todo_Dom.js: added for loop to iterate dom color for ech li element. also added click listener

--- todo_Dom/todo_Dom.js
+++ todo_Dom/todo_Dom.js
@@ -1,11 +1,28 @@
-alert("connected to todo_Dom");
+//alert("connected to todo_Dom");
 
-var firstLI = document.querySelector("LI");
+//var firstLI = document.querySelector("LI");
 
-firstLI.addEventListener("mouseover", function(){
-	firstLI.style.color = "green";
-});
+// it is important to use querySelectorAll her to get all of the Li elements
+var lis = document.querySelectorAll("Li");
 
-firstLI.addEventListener("mouseout", function(){
-	firstLI.style.color = "black";
-});
+
+// NOw we need a for loop to iterate through each element
+for(var i=0; i< lis.length; i++){
+	lis[i].addEventListener("mouseover", function(){
+		this.style.color = "green";
+	});
+	lis[i].addEventListener("mouseout", function(){
+		this.style.color = "black";
+	});
+	lis[i].addEventListener("click", function(){
+			this.classList.toggle("done");
+	});
+}
+
+// firstLI.addEventListener("mouseover", function(){
+// 	firstLI.style.color = "green";
+// });
+
+// firstLI.addEventListener("mouseout", function(){
+// 	firstLI.style.color = "black";
+// });
