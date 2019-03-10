a0069b2da4de2d069da97fc3a7f711047dd36c82
ftemplin <Fritz.templin@gmail.com>
2019-03-09 21:06:22 -0800

    todo_Dom.css added selected class to set style green on hover

--- todo_Dom/todo_Dom.js
+++ todo_Dom/todo_Dom.js
@@ -9,10 +9,16 @@ var lis = document.querySelectorAll("Li");
 // NOw we need a for loop to iterate through each element
 for(var i=0; i< lis.length; i++){
 	lis[i].addEventListener("mouseover", function(){
-		this.style.color = "green";
+		this.classList.add("selected"); 
+
+	// lis[i].addEventListener("mouseover", function(){
+	// 	this.style.color = "green";
 	});
 	lis[i].addEventListener("mouseout", function(){
-		this.style.color = "black";
+		this.classList.remove("selected"); 
+
+	// lis[i].addEventListener("mouseout", function(){
+	// 	this.style.color = "black";
 	});
 	lis[i].addEventListener("click", function(){
 			this.classList.toggle("done");
