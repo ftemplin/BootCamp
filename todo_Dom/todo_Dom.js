98777683479c389c5f7e0e745a2a2a3c6b208a7a
ftemplin <Fritz.templin@gmail.com>
2019-03-09 20:38:59 -0800

    todo_Dom.js, todo_Dom.html: set up hover effect with mouseover and mouseout

--- todo_Dom/todo_Dom.js
+++ todo_Dom/todo_Dom.js
@@ -1,7 +1,11 @@
-a;ert("connected to todo_Dom");
+alert("connected to todo_Dom");
 
 var firstLI = document.querySelector("LI");
 
 firstLI.addEventListener("mouseover", function(){
 	firstLI.style.color = "green";
+});
+
+firstLI.addEventListener("mouseout", function(){
+	firstLI.style.color = "black";
 });
