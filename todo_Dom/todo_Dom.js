9b047c91dc0108134ed0e0bfefae46cfa68f0e11
ftemplin <Fritz.templin@gmail.com>
2019-03-10 15:49:29 -0700

    created Color_Guessing_Game

--- todo_Dom/todo_Dom.js
+++ todo_Dom/todo_Dom.js
@@ -8,17 +8,19 @@ var lis = document.querySelectorAll("Li");
 
 // NOw we need a for loop to iterate through each element
 for(var i=0; i< lis.length; i++){
+	// this code was refactored from below to keep style controls in the css
 	lis[i].addEventListener("mouseover", function(){
 		this.classList.add("selected"); 
 
 	// lis[i].addEventListener("mouseover", function(){
 	// 	this.style.color = "green";
 	});
+	// this code was refactored form below to keep style controls in the css
 	lis[i].addEventListener("mouseout", function(){
 		this.classList.remove("selected"); 
-
-	// lis[i].addEventListener("mouseout", function(){
-	// 	this.style.color = "black";
+	// thecode below was teh original manipulation changed to give control to css above
+		// lis[i].addEventListener("mouseout", function(){
+		// 	this.style.color = "black";
 	});
 	lis[i].addEventListener("click", function(){
 			this.classList.toggle("done");
